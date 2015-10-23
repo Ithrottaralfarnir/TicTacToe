47ac04a74315ee6965970a22b6853b35bb888304
Ingimar <ingimar9@gmail.com>
2015-10-23 13:57:35 +0000

    Added skeleton for TicTacToeService and Test

---
+++ src/test/java/is/ru/TicTacToe/TicTacToeServiceTest.java
@@ -0,0 +1,12 @@
+package is.ru.TicTacToe;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class TicTacToeServiceTest{
+
+  public static void main(String args[]) {
+    org.junit.runner.JUnitCore.main("is.ru.TicTacToe.TicTacToeServiceTest");
+  }
+
+}
