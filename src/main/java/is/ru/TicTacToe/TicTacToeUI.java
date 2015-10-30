9d139af1d1c1ea3367532a2121f5d0fa5f866286
Bjarni Gudmundsson <bjarnigud@i-7f3b5dd9.csd07cloud.internal>
2015-10-30 17:17:22 +0000

    Added displayboard to the UI

--- src/main/java/is/ru/TicTacToe/TicTacToeUI.java
+++ src/main/java/is/ru/TicTacToe/TicTacToeUI.java
@@ -1,5 +1,22 @@
 package is.ru.TicTacToe;
 
 public class TicTacToeUI{
-
+TicTacToeService service = new TicTacToeService();
+	
+	public void displayBoard(){
+		System.out.println("Current Tic-Tac-Toe status");
+		System.out.print("--------------");
+		System.out.println();
+		System.out.println();
+		for(int x = 0; x<3; x++){
+			System.out.print("| ");
+			for(int y = 0; y<3; y++){
+				System.out.print(service.getCell(x, y) + " | ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		System.out.print("--------------");
+		System.out.println();
+	}
 }
