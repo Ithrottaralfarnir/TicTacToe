8a2b0b60c07512dead203080ace1e76453a9ea47
Ingimar <ingimar9@gmail.com>
2015-10-30 16:00:06 +0000

    Added test for initializeBoard

--- src/main/java/is/ru/TicTacToe/TicTacToeService.java
+++ src/main/java/is/ru/TicTacToe/TicTacToeService.java
@@ -1,5 +1,29 @@
 package is.ru.TicTacToe;
 
 public class TicTacToeService{
+  	private char board[][];
+	private char currentPlayer;
+	private int SIZE_OF_BOARD = 3;
+	
+	private final char PLAYER1 = 'X';
+	private final char PLAYER2 = 'O';
+	private final char EMPTY = ' ';
+	
+	public TicTacToeService(){
+		board = new char[SIZE_OF_BOARD][SIZE_OF_BOARD];
+		currentPlayer = PLAYER1;
+		initializeBoard();
+	}
+	
+	private void initializeBoard(){
+		for(int x = 0; x < SIZE_OF_BOARD; x++){
+			for(int y = 0; y < SIZE_OF_BOARD; y++){
+				board[x][y] = EMPTY;
+			}
+		}
+	}
 
+	public char getCell(int x, int y){
+		return board[x][y];
+	}  
 }
--- src/test/java/is/ru/TicTacToe/TicTacToeServiceTest.java
+++ src/test/java/is/ru/TicTacToe/TicTacToeServiceTest.java
@@ -8,5 +8,20 @@ public class TicTacToeServiceTest{
   public static void main(String args[]) {
     org.junit.runner.JUnitCore.main("is.ru.TicTacToe.TicTacToeServiceTest");
   }
+  
+  private int SIZE_OF_BOARD = 3;
 
+  @Test
+  public void testInitializeBoard() {
+        TicTacToeService service = new TicTacToeService();
+        boolean allEmpty = true;
+        for(int x = 0; x < SIZE_OF_BOARD; x++){
+        	for(int y = 0; y < SIZE_OF_BOARD; y++){
+        		if(service.getCell(x, y) != ' '){
+        			allEmpty = false;
+        		}
+        	}
+        }
+        assertEquals(true, allEmpty);
+  }
 }
